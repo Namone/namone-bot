0f23bfe109a9b0f3372e00b1558b0d998ed96ee5
Alex Nordhausen <anorthhouse@gmail.com>
2019-01-04 23:10:12 -0700

    fix: fixing perms

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
   app.on('pull_request.opened', async (context) => {
-    const pull_requestComment = context.issue({ body: 'Task is here.' })
-    await context.github.issues.createComment(pull_requestComment)
+    // const pull_requestComment = context.issue({ body: 'Task is here.' })
+    app.log(context);
   })
 }
