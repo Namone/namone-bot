25169d11b5929566519d345b20f6a26747b78f79
Alex Nordhausen <anorthhouse@gmail.com>
2019-01-05 00:52:26 -0700

    update: updating logic for PR

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,20 @@ import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
   app.on('pull_request.opened', async (context) => {
-    // const pull_requestComment = context.issue({ body: 'Task is here.' })
-    app.log(context);
+    const pattern = /\b[a-zA-Z]{3}\-{1}\d{3}\b/g;
+    const { number, title, user: { login }, head: { repo: { name }}, ...remaining } = context.payload.pull_request;
+
+    const isMatch = title.match(pattern);
+    if (!isMatch)
+      return await app.log("Improper title task ID: " + title);
+
+    const body = "<a href='https://bluetent.atlassian.net/browse/" + isMatch[0].toUpperCase() + "'>This is the task.</a>";
+    // Post a comment for the PR body
+    await context.github.pullRequests.update({
+      repo: name,
+      owner: login,
+      number: number,
+      body: body,
+    });
   })
 }
